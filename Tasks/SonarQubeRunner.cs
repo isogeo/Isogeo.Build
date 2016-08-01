df0d736e5740fbf3f337a779f1180fb8dbdfc495
Mathieu Cartoixa <mathieu.cartoixa@isogeo.fr>
2016-08-01 18:33:37 +0200

    Add SonarQubeRunner task

---
+++ Tasks/SonarQubeRunner.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Isogeo.Build.Tasks
+{
+    public class SonarQubeRunner:
+        ToolTask
+    {
+
+        protected override string GenerateCommandLineCommands()
+        {
+            var builder = new CommandLineBuilder();
+
+            builder.AppendSwitchUnquotedIfNotNull("", Action.ToString().ToLowerInvariant());
+            builder.AppendSwitchIfNotNull("/k: ", ProjectKey);
+            builder.AppendSwitchIfNotNull("/n: ", ProjectName);
+            builder.AppendSwitchIfNotNull("/v: ", ProjectVersion);
+
+            string options = Environment.GetEnvironmentVariable("SONAR_SCANNER_OPTS");
+            if (!string.IsNullOrWhiteSpace(options))
+            {
+                //TODO: refine the parsing of options
+                foreach(var opt in options.Split(' '))
+                    builder.AppendSwitchUnquotedIfNotNull("", Regex.Replace(opt, "^-D", "/d:"));
+            }
+
+            return builder.ToString();
+        }
+
+        protected override string GenerateFullPathToTool()
+        {
+            string path = null;
+
+            if (!string.IsNullOrEmpty(ToolPath))
+                path=Path.GetDirectoryName(Path.GetFullPath(ToolPath));
+
+            return Path.Combine(path, ToolExe);
+        }
+
+        [Required]
+        public string Action
+        {
+            get
+            {
+                return _Action.ToString().ToLowerInvariant();
+            }
+            set
+            {
+                _Action=(SonarQubeAction)Enum.Parse(typeof(SonarQubeAction), value.Replace("-", ""), true);
+            }
+        }
+
+        public string ProjectKey { get; set; }
+
+        public string ProjectName { get; set; }
+
+        public string ProjectVersion { get; set; }
+
+        protected override string ToolName {  get { return "MSBuild.SonarQube.Runner.exe"; } }
+
+        private SonarQubeAction _Action;
+    }
+
+    public enum SonarQubeAction
+    {
+        Begin,
+        End
+    }
+}
